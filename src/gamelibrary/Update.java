e2a643986f2bce622a455ed7bed087ad370c93a2
unknown <berbecaru.alex.ionut@gmail.com>
2018-05-26 08:37:16 +0300

    Nothing was implemented

--- src/gamelibrary/Update.java
+++ src/gamelibrary/Update.java
@@ -7,6 +7,10 @@ package gamelibrary;
 
 import java.io.*;
 import java.sql.*;
+import javax.activation.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
 
 /**
  *
@@ -22,10 +26,16 @@ public class Update {
     }
     
     
-    public static void main(String[] args) throws SQLException{
-        Connection c1=DriverManager.getConnection("jdbc:mtsql://localhost:3306/Alex");
-        Statement s=c1.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
-        ResultSet rs=s.executeQuery("SELECT * FROM library");
+    public static void main(String[] args) throws SQLException, NamingException{
+       Context context = new InitialContext();
+        DataSource dataSource = (DataSource) context.lookup("java:comp/env/jdbc/myDB");
+        Connection c1;
+        c1 = dataSource.getConnection("jdbc:mtsql://localhost:3306/mydata");
+        //Statement s=c1.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_UPDATABLE);
+        //ResultSet rs=s.executeQuery("SELECT * FROM library");
+        Statement s1=c1.createStatement();
+        ResultSet rs=s1.executeQuery("SELECT * FROM Test");
+        System.out.println(rs);
     } 
     
 }
